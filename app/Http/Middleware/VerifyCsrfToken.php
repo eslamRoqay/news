44347c0fa1138b61e1e3de7732715802c1b51e2d
eslamRoqay <102512020+eslamRoqay@users.noreply.github.com>
2022-07-07 12:55:10 +0200

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,8 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-use Illuminate\Contracts\Encryption\Encrypter;
-use Illuminate\Contracts\Foundation\Application;
+
 class VerifyCsrfToken extends Middleware
 {
     /**
@@ -12,13 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    public function __construct(Application $app, Encrypter $encrypter)
-    {
-        parent::__construct($app, $encrypter);
-        $this->except = [
-            route('logout')
-        ];
-    }
+    
     protected $except = [
         //
     ];
