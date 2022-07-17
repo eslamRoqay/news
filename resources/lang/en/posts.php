3300f9f0ae98f16af17579eece0522ea700cfcc3
eslamRoqay <102512020+eslamRoqay@users.noreply.github.com>
2022-07-17 09:01:40 +0200

    Create posts.php

---
+++ resources/lang/en/posts.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'posts' => 'Posts',
+    'title_ar' => 'Title In Arabic',
+    'content_ar' => 'Content In Arabic',
+    'title_en' => 'Title In English',
+    'content_en' => 'Content In English',
+    'image' => 'Image',
+    'action' => 'Action',
+];
