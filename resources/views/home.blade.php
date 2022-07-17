b7327e2f8cf3bdbf34d246f622c8df8f36c95ab2
eslamRoqay <102512020+eslamRoqay@users.noreply.github.com>
2022-07-17 15:16:42 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,21 +40,19 @@
             <div class="card card-custom gutter-b" style="height: 150px">
                 <div class="card-body">
                     <span class="svg-icon svg-icon-success svg-icon-3x">
-                        <!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Group.svg-->
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
-                             height="24px" viewBox="0 0 24 24" version="1.1">
-                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <polygon points="0 0 24 0 24 24 0 24"></polygon>
-                                <path
-                                    d="M18,14 C16.3431458,14 15,12.6568542 15,11 C15,9.34314575 16.3431458,8 18,8 C19.6568542,8 21,9.34314575 21,11 C21,12.6568542 19.6568542,14 18,14 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z"
-                                    fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
-                                <path
-                                    d="M17.6011961,15.0006174 C21.0077043,15.0378534 23.7891749,16.7601418 23.9984937,20.4 C24.0069246,20.5466056 23.9984937,21 23.4559499,21 L19.6,21 C19.6,18.7490654 18.8562935,16.6718327 17.6011961,15.0006174 Z M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z"
-                                    fill="#000000" fill-rule="nonzero"></path>
-                            </g>
-                        </svg>
-                        <!--end::Svg Icon-->
-                    </span>
+
+    <svg
+        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+        viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <rect x="0" y="0" width="24" height="24"/>
+        <path
+            d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z"
+            fill="#000000" fill-rule="nonzero"
+            transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) "/>
+        <rect fill="#000000" opacity="0.3" x="5" y="20" width="15" height="2" rx="1"/>
+    </g>
+</svg><!--end::Svg Icon--></span>
                     <div class="text-dark font-weight-bolder font-size-h2 mt-3">{{$data['posts']}}</div>
                     <a class="text-muted  font-weight-bold font-size-lg mt-1">{{trans('lang.postCount')}}</a>
                 </div>
@@ -69,7 +67,8 @@
                     <h3 class="card-title font-weight-bolder text-dark">{{trans('lang.recent_users')}}</h3>
                     <div class="card-toolbar">
                         <div class="dropdown dropdown-inline">
-                            <a href="{{route('users')}}" class="btn btn-success font-weight-bolder font-size-sm mr-3">{{trans('lang.all_users')}}</a>
+                            <a href="{{route('users')}}"
+                               class="btn btn-success font-weight-bolder font-size-sm mr-3">{{trans('lang.all_users')}}</a>
                         </div>
                     </div>
                 </div>
