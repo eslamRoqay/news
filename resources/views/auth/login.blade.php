0179370783320e073c7b139b82d501797dca550e
eslamRoqay <102512020+eslamRoqay@users.noreply.github.com>
2022-07-18 09:25:48 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@ License: You must have a valid license purchased only from themeforest(the above
 -->
 <html lang="ar" direction="rtl" dir="rtl" style="direction: rtl" >
 <!--begin::Head-->
-<head><base href="../../../../">
+<head>
     <meta charset="utf-8" />
     <title>{{settings('site_name_ar')}}</title>
     <meta name="description" content="Singin page example" />
@@ -24,20 +24,20 @@ License: You must have a valid license purchased only from themeforest(the above
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Page Custom Styles(used by this page)-->
-    <link href="assets/css/pages/login/login-4.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/css/pages/login/login-4.rtl.css" rel="stylesheet" type="text/css" />
     <!--end::Page Custom Styles-->
     <!--begin::Global Theme Styles(used by all pages)-->
-    <link href="assets/plugins/global/plugins.bundle.rtl.css" rel="stylesheet" type="text/css" />
-    <link href="assets/plugins/custom/prismjs/prismjs.bundle.rtl.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/style.bundle.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/plugins/global/plugins.bundle.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/plugins/custom/prismjs/prismjs.bundle.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/css/style.bundle.rtl.css" rel="stylesheet" type="text/css" />
     <!--end::Global Theme Styles-->
     <!--begin::Layout Themes(used by all pages)-->
-    <link href="assets/css/themes/layout/header/base/light.rtl.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/themes/layout/header/menu/light.rtl.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/themes/layout/brand/dark.rtl.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/themes/layout/aside/dark.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/css/themes/layout/header/base/light.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/css/themes/layout/header/menu/light.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/css/themes/layout/brand/dark.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="{{url('/')}}/assets/css/themes/layout/aside/dark.rtl.css" rel="stylesheet" type="text/css" />
     <!--end::Layout Themes-->
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <link rel="shortcut icon" href="{{url('/')}}/assets/media/logos/favicon.ico" />
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Tajawal:wght@800&display=swap');
     </style>
@@ -116,12 +116,12 @@ License: You must have a valid license purchased only from themeforest(the above
 <script>var KTAppSettings = { "breakpoints": { "sm": 576, "md": 768, "lg": 992, "xl": 1200, "xxl": 1400 }, "colors": { "theme": { "base": { "white": "#ffffff", "primary": "#3699FF", "secondary": "#E5EAEE", "success": "#1BC5BD", "info": "#8950FC", "warning": "#FFA800", "danger": "#F64E60", "light": "#E4E6EF", "dark": "#181C32" }, "light": { "white": "#ffffff", "primary": "#E1F0FF", "secondary": "#EBEDF3", "success": "#C9F7F5", "info": "#EEE5FF", "warning": "#FFF4DE", "danger": "#FFE2E5", "light": "#F3F6F9", "dark": "#D6D6E0" }, "inverse": { "white": "#ffffff", "primary": "#ffffff", "secondary": "#3F4254", "success": "#ffffff", "info": "#ffffff", "warning": "#ffffff", "danger": "#ffffff", "light": "#464E5F", "dark": "#ffffff" } }, "gray": { "gray-100": "#F3F6F9", "gray-200": "#EBEDF3", "gray-300": "#E4E6EF", "gray-400": "#D1D3E0", "gray-500": "#B5B5C3", "gray-600": "#7E8299", "gray-700": "#5E6278", "gray-800": "#3F4254", "gray-900": "#181C32" } }, "font-family": "Poppins" };</script>
 <!--end::Global Config-->
 <!--begin::Global Theme Bundle(used by all pages)-->
-<script src="assets/plugins/global/plugins.bundle.js"></script>
-<script src="assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
-<script src="assets/js/scripts.bundle.js"></script>
+<script src="{{url('/')}}/assets/plugins/global/plugins.bundle.js"></script>
+<script src="{{url('/')}}/assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
+<script src="{{url('/')}}/assets/js/scripts.bundle.js"></script>
 <!--end::Global Theme Bundle-->
 <!--begin::Page Scripts(used by this page)-->
-<script src="assets/js/pages/custom/login/login-4.js"></script>
+<script src="{{url('/')}}/assets/js/pages/custom/login/login-4.js"></script>
 <!--end::Page Scripts-->
 </body>
 <!--end::Body-->
